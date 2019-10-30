749a65de6d9dabd65a251d049b24ee7bfc5573fb
Michael Wanyoike <micwanyoike@gmail.com>
2019-10-30 15:39:56 +0300

    refactored main.post tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,34 @@
 from flask import url_for
 
 
-def test_posts(client, auth):
-    # should not access posts without logging in
-    response = client.get(url_for('main.index'), follow_redirects=True)
-    assert b'Sign In' in response.data
+def post(client, post, redirect=False):
+    return client.post(url_for('main.index'),
+                       data={"post": post},
+                       follow_redirects=redirect)
 
-    # should access home page as logged in user
+
+def test_posts(client, auth):
+    # unautheticated user should NOT access home page
+    response = client.get(url_for('main.index'))
+    assert response.status_code == 302
+    assert response.location == 'http://localhost.test/auth/login?next=%2Findex'
+
+    # unauthenticated user should NOT be allowed to post
+    response = post(client, "This is a random message")
+    assert response.status_code == 302
+    assert response.location == 'http://localhost.test/auth/login?next=%2Findex'
+
+    # unautheticated user should NOT access explore page
+    response = client.get(url_for('main.explore'))
+    assert response.status_code == 302
+    assert response.location == 'http://localhost.test/auth/login?next=%2Fexplore'
+
+    # authenticated user should access home page
     auth.login()
     response = client.get(url_for('main.index'), follow_redirects=True)
     assert b'Hi, test!' in response.data
     assert b'This is a random message' not in response.data
 
-    # should post random message
-    response = client.post(url_for('main.index'),
-                           data={"post": "This is a random message"},
-                           follow_redirects=True)
+    # authenticated user should be allowed to post
+    response = post(client, "This is a random message", True)
     assert b'This is a random message' in response.data
